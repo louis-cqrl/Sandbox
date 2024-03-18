7dd8c431072c2aac3f7c420a73ffa11341803b8b
Louis Coquerelle <louis.coquerelle@datadoghq.com>
2024-03-18 09:39:36 +0100

    fix(http-request/test_flare_head.go): check for redirection

--- http-request/test_flare_head.go
+++ http-request/test_flare_head.go
@@ -6,10 +6,23 @@ import (
 	"net/http"
 )
 
-func testWithHTTPHead(url string) {
-	res, err := http.Head(url)
+func acceptRedirection(url string, client *http.Client) (bool, error) {
+	res, err := client.Head(url) // 307 with this function
 	if err != nil {
-		panic(err)
+		return false, err
+	}
+	defer res.Body.Close() // Close the response body
+	if res.StatusCode == http.StatusTemporaryRedirect || res.StatusCode == http.StatusPermanentRedirect || res.StatusCode == http.StatusOK {
+		return true, nil
+	}
+	return false, nil
+}
+
+func testWithHTTPHead(url string, client *http.Client) {
+
+	res, err := client.Head(url) // 307 with this function
+	if err != nil {
+		fmt.Println(err)
 	}
 	defer res.Body.Close() // Close the response body
 	contentlength := res.ContentLength
@@ -32,14 +45,40 @@ func testWithHTTPNewRequest(url string) {
 		fmt.Println("Error sending request:", err)
 		return
 	}
+	urlString := resp.Request.URL.String() // Assign the result to a variable
+	fmt.Println("URL:", urlString)         // Use the variable
 	defer resp.Body.Close()
 	// Print the response status code
 	fmt.Println("Response Status:", resp.Status)
 }
 
 func main() {
-	url := "https://app.datadoghq.com/support/flare/0?api_key=abcdef"
-	testWithHTTPHead(url)
+	client := &http.Client{}
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	url := "https://7-53-0-flare.agent.datadoghq.com/support/flare"
+	testWithHTTPHead(url, client)
 	fmt.Println("======================================================")
 	testWithHTTPNewRequest(url)
+	fmt.Println("======================================================")
+	isRedirect, err := acceptRedirection(url, client)
+	if err != nil || !isRedirect {
+		fmt.Println("Error:", err)
+	} else if isRedirect {
+		fmt.Println("Redirected successfully")
+	}
+	fmt.Println("==============try with no version url=================")
+	ulr2 := "https://app.datadoghq.com//support/flare"
+	testWithHTTPHead(ulr2, client)
+	fmt.Println("======================================================")
+	testWithHTTPNewRequest(ulr2)
+	fmt.Println("======================================================")
+	isRedirect, err = acceptRedirection(ulr2, client)
+	if err != nil || !isRedirect {
+		fmt.Println("Error:", err)
+	} else if isRedirect {
+		fmt.Println("Redirected successfully")
+	}
 }
